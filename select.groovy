53fa0869cc6f05a7150845b6bde7a4a1a1235ff1
sarnobat <ss401533@gmail.com>
2013-07-21 18:59:23 -0700

    Initial commit

--- select.groovy
+++ select.groovy
@@ -17,25 +17,16 @@ import org.apache.commons.dbutils.handlers.*;
 */
 
 Class.forName("org.sqlite.JDBC");
-
-//Connection connection = null;
-//connection = DriverManager.getConnection("jdbc:sqlite:students.db");
-
-
 BasicDataSource dataSource = new BasicDataSource();
 dataSource.setDriverClassName("org.sqlite.JDBC");
 dataSource.setUrl("jdbc:sqlite:students.db");
       
 
-// Create a QueryRunner that will use connections from
-// the given DataSource
 QueryRunner run = new QueryRunner(dataSource);
-
-// Execute the query and get the results back from the handler
 Map[] result = run.query("SELECT name,raised,correct FROM students", new MapListHandler());
 //Map.Entry[] result = run.query("SELECT name,raised FROM students WHERE name=?", new MapHandler(), "Sridhar");
-println(result);
-println(result[0].getClass().getName());
+//println(result);
+//println(result[0].getClass().getName());
 //println(result[0].getKey());
 
 for (Map row : result) {
